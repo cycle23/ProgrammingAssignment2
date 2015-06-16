56af84eb5972d32837513d60a078000b54b394c2
Cycle23 <cycle23x@gmail.com>
2015-06-15 22:33:17 -0700

    Add plenty of comments and also remove the debug message statement, as it is not stated in the requirements.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,20 @@
 
 ## makeCacheMatrix: Create a "matrix" object to cache inverse result.
 makeCacheMatrix <- function(x = matrix()) {
+  # initially no inverse value
   i <- NULL
+  # will 'memento' the matrix input 'y' to internal 'x'
   set <- function(y) {
     x <<- y
     i <<- NULL
   }
+  # returns our local 'x'
   get <- function() x
+  # store some value for the inverse 
   setinv <- function(inv) i <<- inv
+  # return the cache inverse value
   getinv <- function() i
+  # define and return the "matrix" list object
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -20,15 +26,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve: Return the inverse of 'inCache', using the matrix cache.                
 cacheSolve <- function(inCache, ...) {
-        ## Return a matrix that is the inverse of 'inCache'
+  # Try to access the cached value
   i <- inCache$getinv()
+  # if we have cache..
   if(!is.null(i)) {
-    message("getting cached data")
+    # return the cached value
     return(i)
   }
+  # otherwise, retrive the stored data
   data <- inCache$get()
+  # calculate the inverse, passing any optional parameters
   i <- solve(data, ...)
+  # set the calculated value to cache
   inCache$setinv(i)
+  # return the calculated value
   i
 }
 
